0344836a214a38f9067441a8fc3fc9c0b274bcf1
mat roberts <mat@moleseyhill.com>
2018-07-05 16:00:03 +0100

    added a common assemblyinfo

---
+++ AssemblyInfo.common.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("GocdTray")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © ")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.*")]
