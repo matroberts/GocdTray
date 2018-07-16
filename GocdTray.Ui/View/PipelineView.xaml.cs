989c1a3cc4d56b346b345f9107e114a590b72dc2
mat roberts <mat@moleseyhill.com>
2018-07-16 21:16:20 +0100

    Got a sort button added to the screen (it doesn't do anything)

--- GocdTray.Ui/View/PipelineView.xaml.cs
+++ GocdTray.Ui/View/PipelineView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GocdTray.Ui.View
 {
@@ -10,6 +11,15 @@ namespace GocdTray.Ui.View
         public PipelineView()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+            //            throw new NotImplementedException();
         }
     }
 }
